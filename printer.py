238e619203486795436d2db7da86bedc5ffb3176
l00n4t1k <l00n4t1k@gmail.com>
2016-08-20 18:35:56 +1200

    Revert "more detail scraping"
    This reverts commit 333aa7663159e23cbf17ff33dbe9a701f3f06b6a.

--- printer.py
+++ printer.py
@@ -1,11 +1,8 @@
-
-class Printer(object):
-    @staticmethod
-    def printer(the_gen, the_list):
-        print()
-        print('-----------------------------------------')
-        print()
-        print('Gen ', the_gen, ' List')
-        print('----------')
-        for datum in the_list:
-            print(datum)
+def printer(the_gen, the_list):
+    print()
+    print('-----------------------------------------')
+    print()
+    print('Gen ', the_gen, ' List')
+    print('----------')
+    for datum in the_list:
+        print(datum)
--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 import requests
+from formatter import Formatter as f
+from printer import printer
 from bs4 import BeautifulSoup
 
 
@@ -9,32 +11,48 @@ class Scraper(object):
     __min = 0
     __max = 0
     __my_formatter = ''
-    __my_printer = ''
 
-    def __init__(self, the_formatter, the_printer):
+    def __init__(self, the_formatter):
         self.__my_formatter = the_formatter
-        self.__my_printer = the_printer
 
     def gen_decider(self):
-        pass
+        gen = self.get_generation()
+        if gen == 1:
+            self.__min = 0
+            self.__max = 151
+        elif gen == 2:
+            self.__min = 151
+            self.__max = 251
+        elif gen == 3:
+            self.__min = 251
+            self.__max = 386
+        elif gen == 4:
+            self.__min = 386
+            self.__max = 493
+        elif gen == 5:
+            self.__min = 493
+            self.__max = 649
+        elif gen == 6:
+            self.__min = 649
+            self.__max = 721
 
     def set_nat_dex(self, the_dex):
-        pass
+        self.__nat_dex = the_dex
 
     def get_nat_dex(self):
-        pass
+        return self.__nat_dex
 
     def set_generation(self, the_gen):
-        pass
+        self.__generation = the_gen
 
     def get_generation(self):
-        pass
+        return self.__generation
 
     def get_min(self):
-        pass
+        return self.__min
 
     def get_max(self):
-        pass
+        return self.__max
 
     def web_scraper(self):
         dex_data = []
@@ -53,5 +71,3 @@ class Scraper(object):
         out = self.__my_formatter.formatter(dex_data2, self.__min, self.__max)
         return out
 
-    def print(self, the_gen, the_list):
-        self.__my_printer.printer(the_gen, the_list)
